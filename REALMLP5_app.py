fffb4687e9c0526e42b53237c3d966b501be9d0b
ebukaxyrus <ebukadimakunne@gmail.com>
2025-06-25 14:45:11 +0100

    Update REALMLP5_app.py

--- REALMLP5_app.py
+++ REALMLP5_app.py
@@ -100,9 +100,9 @@ if username:
 
         score = retrain.predict(X_input_poly)[0]
 
-    if score < 0:
-        st.warning("⚠️ The predicted score is negative. Setting it to 0.")
-        score = 0  # Clip the score to 0
+        if score < 0:
+            st.warning("⚠️ The predicted score is negative. Setting it to 0.")
+            score = 0  # Clip the score to 0
 
 
         # Store data in CSV
